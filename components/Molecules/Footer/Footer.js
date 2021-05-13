df0275a8f7d13f97e87d8ec4daad50958cf54f5e
miltondw <estradamilton2001@gmail.com>
2021-05-13 17:24:07 -0500

    movil responsive from about finish

--- components/Molecules/Footer/Footer.js
+++ components/Molecules/Footer/Footer.js
@@ -1,7 +1,15 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="map"></div>
+      <div className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d841.0577042094379!2d-71.15199705157842!3d8.576827689134955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6487fbec9a2357%3A0x80b1ba2ba4ee17d4!2sBJT%20Shop!5e1!3m2!1ses!2sco!4v1620933014640!5m2!1ses!2sco"
+          width="600"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
       <div className="footer__content">
         <address>
           <div className="footer__address">
@@ -16,7 +24,9 @@ export default function Footer() {
               Cultural Mariano Picón Salas
             </p>
             <h3 className="footer__title">Nuestro horario de atención</h3>
-            <p className="footer__paragraph">8:00 a.m A 12:00 p.m 1:00 p.m A 5:00 p.m</p>
+            <p className="footer__paragraph">
+              8:00 a.m A 12:00 p.m 1:00 p.m A 5:00 p.m
+            </p>
           </div>
         </address>
         <p className="footer__paragraph">&copy; Creado por Industrias BJT</p>
--- pages/conocenos/index.js
+++ pages/conocenos/index.js
@@ -43,6 +43,17 @@ export default function blog() {
                 Ampliamos las actividades empresariales, ofreciendo un excelente
                 servicio técnico profesional en mantenimiento
               </p>
+              <picture className="article-section__picture">
+                <img
+                  className="article-section__image"
+                  src="/images/conocenos/unidad_final-dsk.png"
+                  alt="unidad final bjt"
+                />
+              </picture>
+              <h3 className="article-section__subtitle">
+                <span className="article-section__subtitle--year">12</span>años
+                de experiencia
+              </h3>
             </section>
           </article>
 
@@ -70,6 +81,8 @@ export default function blog() {
                 className="article-section__images"
                 src="/images/conocenos/nelson-bjt.png"
                 alt="nelson ingeniero BJT"
+                width="300"
+                height="300"
               />
             </picture>
           </article>
@@ -77,31 +90,190 @@ export default function blog() {
           <article className="about-article">
             <header className="article-header">
               <h3 className="article-section__subtitle">
-                altos estandares de calidad y tecnología de punta
+                diseños sometidos a pruebas exhautivas
               </h3>
             </header>
             <section className="article-section">
               <p className="article-section__paragraph">
-                Garantizamos altos estándares de calidad, ya que empleamos
-                tecnología de punta en todas las etapas del diseño y fabricación
-                de los sistemas de ordeño.
+                Adicionalmente empleamos los software de diseño industrial de
+                última generación, con el fin de manufacturar productos de
+                insuperable calidad, con piezas dimensionalmente perfectas y de
+                magnifico acabado.
               </p>
               <p className="article-section__paragraph">
-                reUtilizamos técnicas tan complejas como el modelado 3D por
-                medio del diseño asistido por computadoras (C.A.D), simulaciones
-                3D (c.a.e), asi como también manufactura asistida (c.a.m) por
-                medio de maquinaria C.N.C.
+                Todos nuestros productos son probados exhaustivamente y
+                sometidos a las condiciones reales de trabajo por un largo
+                período de tiempo, para comprobar que su durabilidad y
+                eficiencia son óptimos. Certificamos el perfecto funcionamiento
+                de nuestros equipos.
+              </p>
+              <p className="article-section__paragraph">
+                Diseñamos y fabricamos todo tipo de productos adaptable a
+                cualquier sistema de ordeño mecánico. En nuestras tiendas física
+                y virtual usted encontrará una amplia gama de repuestos y
+                productos para su sistema de ordeño, tanto fijo como portátiles,
+                además de una gran variedad de consumibles. Garantizamos la
+                mejor relación calidad / precio del mercado.
               </p>
             </section>
             <picture className="article-section__picture">
               <img
                 className="article-section__images"
-                src="/images/conocenos/nelson-bjt.png"
-                alt="nelson ingeniero BJT"
+                src="/images/conocenos/bomba-1800-bjt.png"
+                alt="bomba-1800 BJT"
+                width="300"
+                height="300"
               />
             </picture>
           </article>
-        
+
+          <article className="about-article">
+            <header className="article-header">
+              <h3 className="article-section__subtitle">objetivos</h3>
+            </header>
+            <section className="article-section">
+              <p className="article-section__paragraph">
+                Industrias BJT C. A, busca posicionarse en el competido mercado
+                internacional, para ofrecer nuestra cartera de productos como
+                bombas de vacío y bombas sanitarias. Nuestros artículos son
+                fabricados cumpliendo con los más altos estándares de calidad.
+              </p>
+              <p className="article-section__paragraph">
+                Su diseño y manufactura parte de un concepto y formato
+                disruptivos, que permitirá a nuestra empresa proyectar al mundo
+                la excelencia y diversidad de sus equipos, además de mostrar el
+                trabajo y esfuerzo en innovación que estamos realizando para
+                diversificar nuestros productos y adaptarlos a los distintos
+                mercados internacionales; de ese modo, los clientes dispondrán
+                de una mayor variedad de artículos de óptima calidad.
+              </p>
+            </section>
+            <picture className="article-section__picture">
+              <img
+                className="article-section__images"
+                src="/images/conocenos/sala-de-ordeno-bjt.png"
+                alt="bomba-1800 BJT"
+                width="300"
+                height="300"
+              />
+            </picture>
+          </article>
+
+          <article className="about-article">
+            <header className="article-header">
+              <h3 className="article-section__subtitle">misión</h3>
+            </header>
+            <section className="article-section">
+              <p className="article-section__paragraph">
+                Diseñar, fabricar y comercializar sistemas de ordeño de la más
+                alta calidad.
+              </p>
+              <p className="article-section__paragraph">
+                Trabajar en conjunto con nuestros clientes para satisfacer sus
+                necesidades, contribuyendo al logro de sus resultados
+                financieros.
+              </p>
+              <p className="article-section__paragraph">
+                Trabajar en conjunto con nuestros clientes para satisfacer sus
+                necesidades, contribuyendo al logro de sus resultados
+                financieros.
+              </p>
+              <p className="article-section__paragraph">
+                Generar valor agregado a nuestros productos, al ofrecer
+                mantenimiento a los sistemas de ordeño mecánico y asesoría
+                técnica a los clientes, con la finalidad de que obtengan los
+                mejores resultados al momento de utilizarlos, a través de un
+                servicio superior e innovador.
+              </p>
+            </section>
+            <picture className="article-section__picture">
+              <img
+                className="article-section__images"
+                src="/images/conocenos/bufalos.png"
+                alt="bomba-1800 BJT"
+                width="300"
+                height="300"
+              />
+            </picture>
+          </article>
+
+          <article className="about-article">
+            <header className="article-header">
+              <h3 className="article-section__subtitle">visión</h3>
+            </header>
+
+            <section className="article-section">
+              <ul class="list__vision">
+                <li class="li__vision">
+                  Lograr el liderazgo a nivel nacional en la fabricación
+                  sistemas de ordeño, logrando ser reconocida como una empresa
+                  de alta calidad gracias a nuestros productos y servicios.
+                </li>
+                <li class="li__vision">
+                  Situarnos como una de las empresas de más rápido crecimiento a
+                  nivel nacional.
+                </li>
+                <li class="li__vision">
+                  Ofrecer un agradable ambiente de trabajo a nuestro personal,
+                  para que se sienta inspirado a dar cada día lo mejor de sí
+                  mismo.
+                </li>
+                <li class="li__vision">
+                  Generar la necesidad de innovar y seguir avanzando para
+                  satisfacer las necesidades de nuestros clientes.
+                </li>
+                <li class="li__vision">
+                  Ofertar una extensa cartera de productos de calidad.
+                </li>
+                <li class="li__vision">
+                  Crear una red de clientes para crear un nexo duradero.
+                </li>
+                <li class="li__vision">
+                  Ser un aliado imprescindible para el ganadero, antes, durante
+                  y después de adquirir nuestros productos.
+                </li>
+              </ul>
+            </section>
+
+            <picture className="article-section__picture">
+              <img
+                className="article-section__images"
+                src="/images/conocenos/Mapa de venezuela-01.svg"
+                alt="bomba-1800 BJT"
+                width="300"
+                height="300"
+              />
+            </picture>
+          </article>
+          
+          <article className="about-article">
+            <header className="article-header">
+              <h3 className="article-section__subtitle">valores</h3>
+            </header>
+            <section className="article-section">
+              <ul class="article-section__valores">
+                <li class="article-section__valor">Responsabilidad</li>
+                <li class="article-section__valor">Calidad</li>
+                <li class="article-section__valor">
+                  Excelencia en el servicio
+                </li>
+                <li class="article-section__valor">Innovación</li>
+                <li class="article-section__valor">Originalidad</li>
+                <li class="article-section__valor">Liderazgo</li>
+                <li class="article-section__valor">Autocrítica</li>
+                <li class="article-section__valor">Colaboración</li>
+                <li class="article-section__valor">Ética profesional</li>
+                <li class="article-section__valor">Motivación</li>
+                <li class="article-section__valor">Trabajo en equipo</li>
+                <li class="article-section__valor">Perseverancia.</li>
+                <li class="article-section__valor">Transparencia</li>
+                <li class="article-section__valor">Proactividad</li>
+                <li class="article-section__valor">
+                  Espíritu emprendedor y dinamismo
+                </li>
+              </ul>
+            </section>
+          </article>
         </div>
       </div>
     </Layaut>
